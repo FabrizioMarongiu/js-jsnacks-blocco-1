75c796013fafdf0d5036f2e1799ae756c6ced9ac
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-03-25 13:35:41 +0100

    Snacks 2

--- script.js
+++ script.js
@@ -26,23 +26,42 @@
 
 // CICLO WHILE
 
-var somma = 0;
+// var somma = 0;
 
-var maxNum = 5;
+// var maxNum = 5;
 
-var counter = 0;
+// var counter = 0;
 
-while ( counter < maxNum){
+// while ( counter < maxNum){
 
-    var numero = parseInt(prompt('Inserisci un numero'));
+//     var numero = parseInt(prompt('Inserisci un numero'));
 
-    console.log('Il numero che hai inserito è: ', numero);
+//     console.log('Il numero che hai inserito è: ', numero);
 
-     somma += numero;
+//      somma += numero;
 
-     console.log('La somma parziale è: ' , somma);
+//      console.log('La somma parziale è: ' , somma);
 
-     counter++;
+//      counter++;
+// }
+
+// console.log('La somma totale dei numeri che hai inserito è: ', somma);
+
+
+
+
+
+
+
+
+
+// Snack 2
+// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+
+var numero = parseInt(prompt('Inserisci un numero. Se è pari verrà stampato, altrimenti verrà stampato il numero successivo a quello che hai inserito'));
+
+if ( numero % 2 === 0 ){
+    console.log('Il numero che hai inserito è pari. Hai inserito il numero: ', numero);
+}else{
+    console.log('Il numero che hai inserito è dispari, il successivo numero è: ', ++numero);
 }
-
-console.log('La somma totale dei numeri che hai inserito è: ', somma);
